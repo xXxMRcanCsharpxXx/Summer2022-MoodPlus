fbf63c344d5319d6c49ee22e973cc09bb8e28561
MiguelRosado-WCCI <M.rosado1725@gmail.com>
2022-08-02 13:59:29 -0400

    init

---
+++ MoodPlus/MoodPlus/Models/ActOfKindness.cs
@@ -0,0 +1,11 @@
+﻿namespace MoodPlus.Models
+{
+    public class ActOfKindness
+    {
+        public int id { get; set; }
+        //recieve or share a random positive encouragment message to a random user
+        public string Quote { get; set; }
+        public List<string> Quotes { get; set; }
+        public int HighScore { get; set; }
+    }
+}
---
+++ MoodPlus/MoodPlus/Models/Patient.cs
@@ -0,0 +1,20 @@
+﻿namespace MoodPlus.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public int Anxiety { get; set; }
+        public int Fatigue { get; set; }
+        public int Happiness { get; set; }
+        public int Loneliness { get; set; }
+        public int Restless { get; set; }
+        public int Hunger { get; set; }
+        public int Sadness { get; set; }
+        public int Overwhelmed { get; set; }
+        public int Agrivated { get; set; }
+        public int Anger { get; set; }
+
+
+
+    }
+}
