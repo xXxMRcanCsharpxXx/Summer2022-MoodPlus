d231d59d5dba859b3d6073ee399fff822fdf8fbf
DinabMorgan <morgan.dinab@gmail.com>
2022-08-09 21:23:37 -0400

    graph part 1

--- MoodPlus/MoodPlus/Controllers/HomeController.cs
+++ MoodPlus/MoodPlus/Controllers/HomeController.cs
@@ -1,15 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MoodPlus.Data;
 using MoodPlus.Models;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace MoodPlus.Controllers
 {
     public class HomeController : Controller
     {
+        public ApplicationDbContext db { get; set; }
+        public UserManager<Account> userManager { get; set; }
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext db, Microsoft.AspNetCore.Identity.UserManager<Account> userManager, ILogger<HomeController> logger)
         {
+            this.db = db;
+            this.userManager = userManager;
             _logger = logger;
         }
 
@@ -32,6 +38,35 @@ namespace MoodPlus.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Graph()
+        {
+            //ViewBag.Theme = "light2";
+            List<Entry> Entries = db.Entries.Skip(Math.Max(0, db.Entries.Count() - 90)).ToList();
+            List<DataPoint> Happiness = new List<DataPoint>();
+            // ViewBag.Anger = new List<DataPoint>();
+            // do this for other emotions
+
+            for (int i = 0; i < Entries.Count; i++)
+            {
+                foreach(MoodRating mood in Entries[i].Moods)
+                {
+                    DataPoint toAdd = new DataPoint(mood.Feeling, mood.Rating, Entries[i].Date);
+                    switch (mood.Feeling)
+                    {
+                        case Feeling.Happiness:
+                            Happiness.Add(toAdd);
+                            break;
+                        //case Feeling.Anger:
+                        //    ViewBag.Anger.Add(toAdd);
+                        //    break;
+                    }
+                }
+            }
+
+            ViewBag.Happiness = JsonConvert.SerializeObject(Happiness);
+
+            return View();
         }
     }
 }
---
+++ MoodPlus/MoodPlus/Models/DataPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace MoodPlus.Models
+{
+    [DataContract]
+    public class DataPoint
+    {
+        
+        public int Feeling { get; set; }
+
+        [DataMember(Name = "y")]
+        public int Rating { get; set; }
+
+        [DataMember(Name = "x")]
+        public DateTime Date { get; set; }
+
+        public DataPoint (Feeling feeling, int rating, DateTime date)
+        {
+            Feeling = (int) feeling;    
+            Rating = rating;  
+            Date = date;
+        }
+    }
+
+}
