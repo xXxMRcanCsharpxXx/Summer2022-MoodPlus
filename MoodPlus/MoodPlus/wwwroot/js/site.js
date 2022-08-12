6b039015efee5b9c1ac4c061ed275d9f3324c9c0
Xavier M <golf4Xavier@gmail.com>
2022-08-12 15:01:05 -0400

    policy privacy changes

--- MoodPlus/MoodPlus/wwwroot/js/site.js
+++ MoodPlus/MoodPlus/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function myFunction() {
+    var x = document.getElementById("policy-body");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
